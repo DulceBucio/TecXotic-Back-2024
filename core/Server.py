8bd12f5b4c5833051ed01436ce726af61a8462a7
DulceBucio <114613877+DulceBucio@users.noreply.github.com>
2024-03-27 08:06:37 -0600

    cleaned repository tests

--- core/Server.py
+++ core/Server.py
@@ -4,28 +4,28 @@ from threading import Thread
 
 app = Flask(__name__)
 
-# Initialize Pixhawk and Serial connections
+# Conexión a la Pixhawk
 try:
     px = Pixhawk(direction='/dev/serial/by-id/usb-ArduPilot_Pixhawk1_380020000A51353338353732-if00')
 except Exception as e:
     print(f"Initialization error: {e}")
 
-# Handle commands
+# Endpoint que recibe información de control de movimiento y ejecuta funciones de la px
 @app.route('/postControlMovement', methods=['POST'])
 def post_control_movement():
     commands = request.json
     
-    # Manual Control Commands
+    # Manejo de movimiento por defecto
     if all(k in commands for k in ('roll', 'pitch', 'yaw', 'throttle')):
         px.drive_manual(commands['roll'], commands['pitch'], commands['yaw'], commands['throttle'], 0)
     
-    # Mode Change Command
+    # Cambio de modo 
     if 'mode' in commands:
         current_mode = px.get_pix_info()['mode']
         if commands['mode'] != current_mode:
             px.change_mode(commands['mode'])
     
-    # Arm/Disarm Command
+    # arm/disarm según el estado actual
     if 'arm_disarm' in commands:
         current_arm_state = px.get_pix_info()['is_armed']
         if commands['arm_disarm'] != current_arm_state:
